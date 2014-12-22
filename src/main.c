72962c777ccdf87a705049902b90bb23f6481e67
Owen Bullock <avocalodge@gmail.com>
2014-12-22 11:52:03 +0000

    GPLv3

--- src/main.c
+++ src/main.c
@@ -1,3 +1,21 @@
+/* ---------------------------------------------------------
+    Pebble Tides UK
+    Copyright (C) 2014 Owen Bullock 
+                       owen.skimapp@gmail.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ---------------------------------------------------------*/
 #include <pebble.h>
 #include <graph.h>
 #include <config.h>  
